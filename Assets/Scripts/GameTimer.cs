6593845c6f4c40f777e1dc93ac01421f62c65024
Mixail <mixail.korchikov@mail.ru>
2021-03-13 20:38:17 +0300

    Time display until app closing

--- Assets/Scripts/GameTimer.cs
+++ Assets/Scripts/GameTimer.cs
@@ -1,19 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameTimer : MonoBehaviour
 {
     [SerializeField] private Network network;
+    [SerializeField] private Image image;
     [Header("Max waiting time for player actions")]
     [SerializeField] [Range(1, 10)] private byte Minutes;
 
+    private int MaxTimeSeconds;
     private int remainingTimeSeconds;
 
     private void Awake()
     {
-        remainingTimeSeconds = Minutes;
-        network.onUserActions.AddListener(() => remainingTimeSeconds = Minutes);
+        MaxTimeSeconds = Minutes * 60;
+        remainingTimeSeconds = MaxTimeSeconds;
+        network.onUserActions.AddListener(() => remainingTimeSeconds = Minutes * 60);
     }
 
     private void Start()
@@ -25,12 +29,13 @@ public class GameTimer : MonoBehaviour
     {
         while (true)
         {
+            image.fillAmount = (float) remainingTimeSeconds / MaxTimeSeconds;
             if (remainingTimeSeconds-- <= 0)
             {
                 Application.Quit();
             }
 
-            yield return new WaitForSeconds(60);
+            yield return new WaitForSeconds(1);
         }
     }
 }
